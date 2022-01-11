4270de9c40734db6ef158c949b62c201b5bd616b
Alex Somera <alexsomera@gmail.com>
2022-01-11 11:17:00 -0300

    Initail commit

---
+++ public/assets/scripts/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./forget");
+require("./login");
+require("./orders");
+require("./pay");
+require("./register");
---
+++ public/assets/scripts/src/index.ts
@@ -0,0 +1,7 @@
+import "./forget";
+import "./login";
+import "./orders";
+import "./pay";
+import "./register";
+
+console.log("Hello");
